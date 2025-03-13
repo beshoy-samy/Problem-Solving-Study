4da17386eed12c6a4ac7924d8c43d6eb61009215
Beshoy Samy <beshoy.samy411@gmail.com>
2025-03-13 11:27:32 +0100

    structy binary trees

--- src/main/kotlin/structy/Trees.kt
+++ src/main/kotlin/structy/Trees.kt
@@ -2,29 +2,42 @@ package structy
 
 import Node
 import QueueArray
-import ifNotEmpty
+import StackArray
 import kotlin.math.max
 
-fun dfs(root: Node<Char>?) {
-    if (root == null) return
-    print(root.value)
-    dfs(root.left)
-    dfs(root.right)
+fun depthFirstValues(root: Node<String>?): List<String> {
+    if (root == null) return listOf()
+    val stack = StackArray<Node<String>>()
+    stack.push(root)
+    val values = mutableListOf<String>()
+    while (stack.isEmpty().not()) {
+        val node = stack.pop()!!
+        values.add(node.value)
+        if (node.right != null) stack.push(node.right!!)
+        if (node.left != null) stack.push(node.left!!)
+    }
+    return values
 }
 
-fun bfs(root: Node<Char>?) {
-    if (root == null) return
+fun dfs(root: Node<String>?, values: MutableList<String> = mutableListOf()): List<String> {
+    if (root == null) return values
+    values.add(root.value)
+    dfs(root.left, values)
+    dfs(root.right, values)
+    return values
+}
 
-    val queue = QueueArray<Node<Char>>()
-    queue.enqueue(root)
-
-    while (queue.isEmpty().not()) {
-        val current = queue.dequeue()!!
-        print(current.value)
-        current.left?.also { queue.enqueue(it) }
-        current.right?.also { queue.enqueue(it) }
+fun breadthFirstValues(root: Node<String>?): List<String> {
+    if (root == null) return listOf()
+    val queue = QueueArray(root)
+    val values = mutableListOf<String>()
+    while (queue.isNotEmpty()) {
+        val node = queue.dequeue()!!
+        values.add(node.value)
+        if (node.left != null) queue.enqueue(node.left!!)
+        if (node.right != null) queue.enqueue(node.right!!)
     }
-
+    return values
 }
 
 fun treeSum(root: Node<Int>?): Int {
@@ -32,113 +45,102 @@ fun treeSum(root: Node<Int>?): Int {
     return root.value + treeSum(root.left) + treeSum(root.right)
 }
 
-fun <T> treeIncludes(root: Node<T>?, value: T): Boolean {
+fun treeIncludes(root: Node<String>?, target: String): Boolean {
     if (root == null) return false
-
-    if (root.value == value) return true
-
-    return treeIncludes(root.left, value) || treeIncludes(root.right, value)
+    if (root.value == target) return true
+    return treeIncludes(root.left, target) or treeIncludes(root.right, target)
 }
 
-fun treeMinValue(root: Node<Int>?): Int? {
-    if (root == null) return null
-
-    return minOf(root.value, treeMinValue(root.left) ?: Int.MAX_VALUE, treeMinValue(root.right) ?: Int.MAX_VALUE)
+fun treeMinValue(root: Node<Double>?): Double {
+    if (root == null) return Double.POSITIVE_INFINITY
+    return minOf(root.value, treeMinValue(root.left), treeMinValue(root.right))
 }
 
-fun maxPathSum(root: Node<Int>?): Int {
-    if (root == null) return Int.MIN_VALUE
-
+fun maxPathSum(root: Node<Double>?): Double {
+    if (root == null) return Double.NEGATIVE_INFINITY
     if (root.left == null && root.right == null) return root.value
-
-    return root.value + max(maxPathSum(root.left), maxPathSum(root.right))
+    return root.value + maxOf(maxPathSum(root.left), maxPathSum(root.right))
 }
 
-
-/**
- * T -> O(n^2) because of the list appending to alternate this we can append normally to the list and build a function on top to reverse it
- * S -> O(n)
- */
-fun <T> pathFinder(root: Node<T>?, target: T): List<T>? {
+fun <T> pathFinder(root: Node<T>?, target: T, path: MutableList<T> = mutableListOf()): List<T>? {
     if (root == null) return null
-
-    if (root.value == target) return listOf(target)
-
-    val left = pathFinder(root.left, target)
-    val right = pathFinder(root.right, target)
-
-    return if (left != null) listOf(root.value) + left else if (right != null) listOf(root.value) + right else null
-}
-
-fun binaryTreePaths(root: Node<Char>?, path: String = String()): MutableList<String> {
-    if (root == null) return mutableListOf()
-
-    if (root.left == null && root.right == null) return mutableListOf(path.ifNotEmpty { "$path->" } + root.value)
-
-    return binaryTreePaths(root.left, path.ifNotEmpty { "$path->" } + root.value).apply {
-        addAll(binaryTreePaths(root.right, path.ifNotEmpty { "$path->" } + root.value))
-    }
+    path.add(root.value)
+    if (root.value == target) return path
+    return pathFinder(root.left, target, path) ?: pathFinder(root.right, target, path)
 }
 
 fun <T> treeValueCount(root: Node<T>?, target: T): Int {
     if (root == null) return 0
-
-    return (if (root.value == target) 1 else 0) + treeValueCount(root.left, target) + treeValueCount(root.right, target)
+    val count = if (root.value == target) 1 else 0
+    return count + treeValueCount(root.left, target) + treeValueCount(root.right, target)
 }
 
 fun <T> howHigh(root: Node<T>?): Int {
     if (root == null) return -1
-
-    return 1 + maxOf(howHigh(root.left), howHigh(root.right))
+    if (root.left == null && root.right == null) return 0
+    val left = howHigh(root.left).inc()
+    val right = howHigh(root.right).inc()
+    return max(left, right)
 }
 
+/*
+ * don't always force to solve in recursion
+ * for example here bfs is most suited
+ */
 fun <T> bottomRightValue(root: Node<T>): T {
     val queue = QueueArray(root)
-
-    var current = root
-    while (queue.isEmpty().not()) {
-        current = queue.dequeue()!!
-
-        if (current.left != null)
-            queue.enqueue(current.left!!)
-
-        if (current.right != null)
-            queue.enqueue(current.right!!)
-
+    var rightBottomNode = root.value
+    while (queue.isNotEmpty()) {
+        val node = queue.dequeue()!!
+        rightBottomNode = node.value
+        node.left?.let { queue.enqueue(it) }
+        node.right?.let { queue.enqueue(it) }
     }
+    return rightBottomNode
+}
 
-    return current.value
+fun <T> allTreePaths(root: Node<T>?, path: List<T> = listOf()): List<List<T>> {
+    if (root == null) return listOf()
+    val currentPath = path + root.value
+    if (root.left == null && root.right == null) return listOf(currentPath)
+    val left = allTreePaths(root.left, currentPath)
+    val right = allTreePaths(root.right, currentPath)
+    return left + right
 }
 
 fun <T> treeLevels(root: Node<T>?): List<List<T>> {
     if (root == null) return listOf()
-
+    val levels = mutableListOf<MutableList<T>>()
     val queue = QueueArray(root to 0)
-    val levels: MutableList<MutableList<T>> = mutableListOf()
-
     while (queue.isNotEmpty()) {
-        val (node, levelIndex) = queue.dequeue()!!
-
-
-        val level = levels.getOrNull(levelIndex) ?: mutableListOf()
-        level.add(node.value)
-        if (levels.size < levelIndex.inc()) {
-            levels.add(levelIndex, level)
-        } else {
-            levels[levelIndex] = level
-        }
-
-        node.left?.let { queue.enqueue(it to levelIndex.inc()) }
-        node.right?.let { queue.enqueue(it to levelIndex.inc()) }
+        val (node, level) = queue.dequeue()!!
+        if (levels.size <= level) levels.add(mutableListOf())
+        levels[level] = levels[level].apply { add(node.value) }
+        node.left?.let { queue.enqueue(it to level.inc()) }
+        node.right?.let { queue.enqueue(it to level.inc()) }
     }
-
     return levels
 }
 
-fun <T> leafValues(root: Node<T>?): List<T> {
+fun levelAverages(root: Node<Int>?): List<Double> {
     if (root == null) return listOf()
+    val levels = mutableListOf<Pair<Int, Int>>()
+    val queue = QueueArray(root to 0)
+    while (queue.isNotEmpty()) {
+        val (node, level) = queue.dequeue()!!
+        if (levels.size <= level) levels.add(node.value to 1)
+        else levels[level] = levels[level].first.plus(node.value) to levels[level].second.inc()
+        node.left?.let { queue.enqueue(it to level.inc()) }
+        node.right?.let { queue.enqueue(it to level.inc()) }
+    }
 
+    val averages = levels.map { (sum, count) -> sum.toDouble().div(count) }
+
+    return averages
+}
+
+fun <T> leafList(root: Node<T>?): List<T> {
+    if (root == null) return listOf()
     if (root.left == null && root.right == null) return listOf(root.value)
-
-    return leafValues(root.left) + leafValues(root.right)
+    return leafList(root.left) + leafList(root.right)
 }
