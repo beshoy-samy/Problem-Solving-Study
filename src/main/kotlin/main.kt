63e3b4d995b296c574b4c20ec0d1538421b90f49
Beshoy Khalil <beshoy.khalil@goflink.com>
2025-05-29 15:29:06 +0300

    structy graphs problems

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,16 +1,18 @@
-import leetcode.balanceBST
-import structy.*
+import structy.prereqsPossible
 
 fun main(args: Array<String>) {
     //printTree(fakeBST)
     //printTree(balanceBST(fakeBSTHard).first)
     println(
-        hasCycle(
-            mapOf(
-                "a" to listOf("b"),
-                "b" to listOf("c"),
-                "c" to listOf("d"),
-                "d" to listOf("b")
+        prereqsPossible(
+            6,
+            listOf(
+                listOf(0, 1),
+                listOf(2, 3),
+                listOf(0, 2),
+                listOf(1, 3),
+                listOf(4, 5),
+                listOf(3, 0)
             )
         )
     )
